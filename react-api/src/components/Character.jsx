c09fd7c5c779103afeab7c1f9e025b688f096723
Chris_Velázquez04 <placebo0704@gmail.com>
2022-07-07 03:21:44 +0000

    final API React Js. Rick And Morty

--- react-api/src/components/Character.jsx
+++ react-api/src/components/Character.jsx
@@ -2,9 +2,10 @@ import React from "react";
 
 export const Character = (character) => {
   return (
-    <div>
-     <h2>{character.name}</h2>
-      <img src={character.image} alt={character.name} />
+    <div className="text-center p-5">
+     <h2 className="fs-4 p-4">{character.name}</h2>
+      <img className="img-fluid rounded-pill" src={character.image} alt={character.name} />
+      <p className="fs-6 ">{character.origin}</p> 
     </div>
   )
 }; 
--- react-api/src/components/CharacterList.jsx
+++ react-api/src/components/CharacterList.jsx
@@ -2,38 +2,59 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { Character } from "./Character";
 
+//** ==== Pagination ==== */
+function NavPage(props) {
+     return(
+       <header className="d-flex justify-content-between align-items-center">
+         <p>Page :{props.page}</p>
+         <button className="btn  btn-primary btn-sm" 
+          onClick={() =>  props.setPage(props.page + 1)}
+          >
+          Page: {props.page + 1}
+         </button>
+       </header>
+     )
+}
+
+//** === Export List ====  */
 export const CharacterList = () => {
   const [characters, setCharacter] = useState([]);
   const [loading, setLoading] = useState(true); 
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch("https://rickandmortyapi.com/api/character");
+      const response = await fetch(`https://rickandmortyapi.com/api/character?page=${page}`);
       const data = await response.json();
       setLoading(false); 
       setCharacter(data.results);
     }
 
     fetchData();
-  }, []);
-
-  if(loading) {
-    return(
-      <div>Loading....</div>
-    )
-  }
+  }, [page]);
 
   return (
     <div className="container">
-      <div className="row">
-        {characters.map((character) => {
-          return (
-            <div className="col-md-4" key={character.id}>
-              <Character name={character.name} image={character.image} />
-            </div>
-          );
-        })}
-      </div>
+      <NavPage page={page} setPage={setPage} />
+
+      {loading ? (
+        <h1>Loading....</h1>
+      ) : (
+        <div className="row">
+          {characters.map((character) => {
+            return (
+              <div className="col-md-4" key={character.id}>
+                <Character
+                  name={character.name}
+                  image={character.image}
+                  origin={character.origin.name}
+                />
+              </div>
+            );
+          })}
+        </div>
+      )}
+      <NavPage page={page} setPage={setPage} />
     </div>
   );
 };
